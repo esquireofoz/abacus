47d5a7f2194dfdb13ed4f7b6d54436419ddf1051
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2020-04-30 13:09:24 -0600

    Remove Need for Env Vars

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
-require('dotenv').config()
 const nextBundleAnalyzer = require('@next/bundle-analyzer')
 
 const withBundleAnalyzer = nextBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 })
 
-const nextJsConfig = {
-  env: {
-    EXPERIMENTS_REST_API_URL_ORIGIN: process.env.EXPERIMENTS_REST_API_URL_ORIGIN,
-    EXPERIMENTS_REST_API_URL_PATH_ROOT: process.env.EXPERIMENTS_REST_API_URL_PATH_ROOT,
-  },
-}
-
-module.exports = withBundleAnalyzer(nextJsConfig)
+module.exports = withBundleAnalyzer({})
