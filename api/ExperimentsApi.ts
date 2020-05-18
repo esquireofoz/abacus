868a7d4b8a639060557736dbe456f303339e5026
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2020-05-18 10:04:28 -0600

    Make API Calls DRYer

--- api/ExperimentsApi.ts
+++ api/ExperimentsApi.ts
@@ -1,9 +1,7 @@
 import { ExperimentBare } from '@/models/index'
-import { getExperimentsAuthInfo } from '@/utils/auth'
 
 import { ApiData } from './ApiData'
-import UnauthorizedError from './UnauthorizedError'
-import { resolveApiUrlRoot, PRODUCTION_API_URL_ROOT } from './utils'
+import { fetchApi } from './utils'
 
 /**
  * Finds all the available experiments.
@@ -13,24 +11,8 @@ import { resolveApiUrlRoot, PRODUCTION_API_URL_ROOT } from './utils'
  * @throws UnauthorizedError
  */
 async function findAll(): Promise<ExperimentBare[]> {
-  const apiUrlRoot = resolveApiUrlRoot()
-
-  let headers
-  if (apiUrlRoot === PRODUCTION_API_URL_ROOT) {
-    const accessToken = getExperimentsAuthInfo()?.accessToken
-    if (!accessToken) {
-      throw new UnauthorizedError()
-    }
-    headers = new Headers({ Authorization: `Bearer ${accessToken}` })
-  }
-
-  const fetchUrl = `${apiUrlRoot}/experiments`
-  return fetch(fetchUrl, {
-    method: 'GET',
-    headers: headers,
-  })
-    .then((response) => response.json())
-    .then((result) => result.experiments.map((apiData: ApiData) => new ExperimentBare(apiData)))
+  const result = await fetchApi('GET', '/experiments')
+  return result.experiments.map((apiData: ApiData) => new ExperimentBare(apiData))
 }
 
 const ExperimentsApi = {
--- api/MetricsApi.ts
+++ api/MetricsApi.ts
@@ -1,9 +1,7 @@
 import { MetricBare } from '@/models/index'
-import { getExperimentsAuthInfo } from '@/utils/auth'
 
 import { ApiData } from './ApiData'
-import UnauthorizedError from './UnauthorizedError'
-import { resolveApiUrlRoot, PRODUCTION_API_URL_ROOT } from './utils'
+import { fetchApi } from './utils'
 
 /**
  * Finds all the available metrics.
@@ -13,24 +11,8 @@ import { resolveApiUrlRoot, PRODUCTION_API_URL_ROOT } from './utils'
  * @throws UnauthorizedError
  */
 async function findAll(): Promise<MetricBare[]> {
-  const apiUrlRoot = resolveApiUrlRoot()
-  const isProduction = apiUrlRoot === PRODUCTION_API_URL_ROOT
-
-  let accessToken
-  if (isProduction) {
-    accessToken = getExperimentsAuthInfo()?.accessToken
-    if (!accessToken) {
-      throw new UnauthorizedError()
-    }
-  }
-
-  const fetchUrl = `${apiUrlRoot}/metrics`
-  return fetch(fetchUrl, {
-    method: 'GET',
-    headers: isProduction ? new Headers({ Authorization: `Bearer ${accessToken}` }) : undefined,
-  })
-    .then((response) => response.json())
-    .then((result) => result.metrics.map((apiData: ApiData) => new MetricBare(apiData)))
+  const result = await fetchApi('GET', '/metrics')
+  return result.metrics.map((apiData: ApiData) => new MetricBare(apiData))
 }
 
 const MetricsApi = {
