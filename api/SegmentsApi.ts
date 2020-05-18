917fa52e8528d44de59a5613f39c3231871906a3
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2020-05-18 10:12:09 -0600

    Make API call DRYer

--- api/SegmentsApi.ts
+++ api/SegmentsApi.ts
@@ -1,9 +1,7 @@
 import { Segment } from '@/models/index'
-import { getExperimentsAuthInfo } from '@/utils/auth'
 
 import { ApiData } from './ApiData'
-import UnauthorizedError from './UnauthorizedError'
-import { resolveApiUrlRoot, PRODUCTION_API_URL_ROOT } from './utils'
+import { fetchApi } from './utils'
 
 /**
  * Finds all the available segments.
@@ -13,24 +11,8 @@ import { resolveApiUrlRoot, PRODUCTION_API_URL_ROOT } from './utils'
  * @throws UnauthorizedError
  */
 async function findAll(): Promise<Segment[]> {
-  const apiUrlRoot = resolveApiUrlRoot()
-  const isProduction = apiUrlRoot === PRODUCTION_API_URL_ROOT
-
-  let accessToken
-  if (isProduction) {
-    accessToken = getExperimentsAuthInfo()?.accessToken
-    if (!accessToken) {
-      throw new UnauthorizedError()
-    }
-  }
-
-  const fetchUrl = `${apiUrlRoot}/segments`
-  return fetch(fetchUrl, {
-    method: 'GET',
-    headers: isProduction ? new Headers({ Authorization: `Bearer ${accessToken}` }) : undefined,
-  })
-    .then((response) => response.json())
-    .then((result) => result.segments.map((apiData: ApiData) => new Segment(apiData)))
+  const result = await fetchApi('GET', '/segments')
+  return result.segments.map((apiData: ApiData) => new Segment(apiData))
 }
 
 const SegmentsApi = {
